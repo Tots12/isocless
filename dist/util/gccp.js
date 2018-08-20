4ea6f713f0f09b662a8f1598c12480b8722c321c
Tots12 <42364195+Tots12@users.noreply.github.com>
2018-08-19 22:23:24 -0300

    2.1.2

--- dist/util/gccp.js
+++ dist/util/gccp.js
@@ -3,20 +3,25 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 function gccp(t) {
     let params = Reflect.getMetadata('design:paramtypes', t);
-    params.forEach((param, i) => {
-        if (param) {
-            if (param.toString().includes("{") && param.toString().includes(" ")) {
-                params[i] = param.toString()
-                    .split("{")[0]
-                    .split(" ")[1];
+    if (params) {
+        params.forEach((param, i) => {
+            if (param) {
+                if (param.toString().includes("{") && param.toString().includes(" ")) {
+                    params[i] = param.toString()
+                        .split("{")[0]
+                        .split(" ")[1];
+                }
+                if (params[i].toString().includes("(") && params[i].toString().includes(")")) {
+                    params[i] = param.toString()
+                        .split("(")[0]
+                        .split("function ")[1];
+                }
             }
-            if (params[i].toString().includes("(") && params[i].toString().includes(")")) {
-                params[i] = param.toString()
-                    .split("(")[0]
-                    .split("function ")[1];
-            }
-        }
-    });
+        });
+    }
+    else {
+        throw new Error("You need to use the decorators in all the providers, and in all the commands to work!");
+    }
     return params;
 }
 exports.gccp = gccp;
--- src/util/gccp.ts
+++ src/util/gccp.ts
@@ -3,21 +3,25 @@ import 'reflect-metadata';
 export function gccp(t: object): string[] {
     let params: string[] = Reflect.getMetadata('design:paramtypes', t);
 
-    params.forEach((param, i) => {
-        if (param) {
-            if (param.toString().includes("{") && param.toString().includes(" ")) {
-                params[i] = param.toString()
-                    .split("{")[0]
-                    .split(" ")[1];
+    if (params) {
+        params.forEach((param, i) => {
+            if (param) {
+                if (param.toString().includes("{") && param.toString().includes(" ")) {
+                    params[i] = param.toString()
+                        .split("{")[0]
+                        .split(" ")[1];
+                }
+        
+                if (params[i].toString().includes("(") && params[i].toString().includes(")")) {
+                    params[i] = param.toString()
+                        .split("(")[0]
+                        .split("function ")[1];
+                }
             }
-    
-            if (params[i].toString().includes("(") && params[i].toString().includes(")")) {
-                params[i] = param.toString()
-                    .split("(")[0]
-                    .split("function ")[1];
-            }
-        }
-    });
+        });
+    } else {
+        throw new Error("You need to use the decorators in all the providers, and in all the commands to work!");
+    }
 
     return params;
 }
